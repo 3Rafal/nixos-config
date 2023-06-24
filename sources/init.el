b6f7fcdd44f3619ef76d292ba5d782408cc2efd0
Rafał Gwoździńsi <rafal@gwozdzinski.com>
2023-06-24 09:09:57 +0200

    Simplify ivy bindings

--- sources/init.el
+++ sources/init.el
@@ -114,18 +114,7 @@
 
 (use-package ivy
   :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
+  :bind (("C-s" . swiper))
   :config
   (ivy-mode 1))
 
